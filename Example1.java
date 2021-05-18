259f956a8814d85e8ede91b55e97b648fc3746d1
sharmistha1999 <84120187+sharmistha1999@users.noreply.github.com>
2021-05-18 19:32:38 +0530

    Add files via upload

---
+++ Example1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stu3
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter the value of a");
+int a = sc.nextInt();
+System.out.println("Enter the value of b");
+int b = sc.nextInt();
+System.out.println("addition="+(a+b));
+
+	}
+
+}
