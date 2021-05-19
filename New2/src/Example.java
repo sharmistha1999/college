f56e4c313e24d6a390f5c0cfee00140972fa4be1
sharmistha1999 <84120187+sharmistha1999@users.noreply.github.com>
2021-05-19 08:26:09 +0530

    Add files via upload

---
+++ New2/src/Example.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter a ");
+		int a=sc.nextInt();
+		System.out.println(a);
+	}
+
+}
